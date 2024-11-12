75fe96959ca6cee3d72be5000f22976617c3281e
guialves50 <guialves.garf50@hotmail.com>
2024-11-11 22:51:59 -0300

    adding express

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-import http from "http";
+// import http from "http";
+import app from "./src/app.js"
 
 const PORT = 3000
 
-const routes = {
-    "/": "Curso de Node.js"
-}
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" })
-    res.end(routes[req.url])
-})
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log('servidor escutando')
 })

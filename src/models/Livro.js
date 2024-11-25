bf9919566c8e4df4fda4204771548d55553e0278
guialves50 <guialves.garf50@hotmail.com>
2024-11-25 19:59:21 -0300

    finish

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -6,7 +6,8 @@ const livroSchema = new mongoose.Schema({
   titulo: { type: String, required: true },
   editora: { type: String },
   preco: { type: Number },
-  paginas: { type: Number }
+  paginas: { type: Number },
+  autor: autorSchema
 }, { versionKey: false });
 
 const livro = mongoose.model("livros", livroSchema);

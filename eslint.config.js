7b200d6d9d0b87f6dc7e5f0a5ad8a5443526de85
guialves50 <guialves.garf50@hotmail.com>
2024-11-26 20:57:27 -0300

    adding eslint

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	{languageOptions: { globals: globals.browser }},
+	pluginJs.configs.recommended,
+
+  {
+    env: {
+      "browser": true,
+      "es2021": true,
+      "node": true
+    },
+		rules: {
+			"no-unused-vars": "error",
+			"no-undef": "error",
+			"quotes": ["error", "double"],
+			"indent": ["error", "tab"],
+			"semi": ["error", "always"],
+			"linebreak-style": ["error", "unix"]
+		}
+	}
+];
